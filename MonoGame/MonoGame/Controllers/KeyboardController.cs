ce53bdcf8cb6d301e09ae42bea46432453159a44
Jack Butts <butts.102@osu.edu>
2015-11-05 14:26:12 -0500

    Added game files

---
+++ MonoGame/MonoGame/Controllers/KeyboardController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGame
+{
+    class KeyboardController : IController
+    {
+        private KeyboardState keyboardState;
+        private ICommands currentCommand;
+        private Dictionary<Keys, ICommands> commandLibrary;
+
+        public KeyboardController()
+        {
+
+        }
+
+        public void Update()
+        {
+            foreach (Keys key in keyboardState.GetPressedKeys())
+            {
+
+            }
+        }
+    }
+}
