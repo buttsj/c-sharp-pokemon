ec6465109046aed31aa12a922b304fad19644289
John (Jack) Butts <butts.102@osu.edu>
2015-11-09 23:44:42 -0500

    Ledge collision failed to upload, reuploading

---
+++ MonoGame/MonoGame/Collision/LedgeCollisionResponse.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MonoGame
+{
+    class LedgeCollisionResponse
+    {
+        Game1 game;
+
+        public LedgeCollisionResponse(Game1 game)
+        {
+            this.game = game;
+        }
+
+        public void PlayerTileCollide(Player player, Ledge ledge)
+        {
+            Rectangle playerBox = player.state.GetBoundingBox(player.position);
+            Rectangle ledgeBox = ledge.GetBoundingBox();
+            Rectangle intersection = Rectangle.Intersect(playerBox, ledgeBox);
+
+            if (intersection.Height > intersection.Width)
+            {
+                if (playerBox.Right > ledgeBox.Left && playerBox.Right < ledgeBox.Right)
+                {
+                    player.position.X -= intersection.Width;
+                }
+                else
+                {
+                    player.position.X += intersection.Width;
+                }
+            }
+            else if (intersection.Height < intersection.Width)
+            {
+                if (playerBox.Bottom > ledgeBox.Top && playerBox.Bottom < ledgeBox.Bottom)
+                {
+                    if (intersection.Height > 1)
+                    {
+                        //player.position.Y -= intersection.Height;
+                    }
+                }
+                else
+                {
+                    player.position.Y += intersection.Height;
+                }
+            }
+        }
+    }
+}
