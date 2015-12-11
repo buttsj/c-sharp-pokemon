f99ebff36697ea02a7a20b319da78d702af291ea
John (Jack) Butts <butts.102@osu.edu>
2015-12-11 01:35:55 -0500

    added missing file

---
+++ MonoGame/MonoGame/Commands/Encounter Function Commands/UseMoveCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace MonoGame
+{
+    class UseMoveCommand : ICommands
+    {
+        Game1 game;
+        Damage dmg;
+        Monster atkMon;
+        Monster enemMon;
+        Move move;
+
+        public UseMoveCommand(Damage dmg, Monster atkMon, Monster enemMon, Move move)
+        {
+            this.dmg = dmg;
+            this.atkMon = atkMon;
+            this.enemMon = enemMon;
+            this.move = move;
+        }
+        
+        public void Execute()
+        {
+            int dmgInt = dmg.CalculateDamage(atkMon, enemMon, move);
+            Debug.WriteLine(dmgInt);
+            enemMon.currHealth -= dmgInt;
+            Debug.WriteLine(enemMon.currHealth);
+
+        }
+    }
+}
